5bcef6d2914244ca27c042eb80efa31a1cab4275
xiayulin123 <xiayulinsky@gmail.com>
2023-07-16 16:07:26 -0400

    waiting for connect

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const socketIO = require('socket.io')(http, {
 
 const PORT = 4000
 
+function createUniqueId () {
+  return Math.random.toString(20).substring(2, 10)
+}
+
 let chatgroups = []
+
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -27,6 +32,27 @@ socketIO.on('connection', (socket) => {
     console.log(currentGroupName)
     chatgroups.unshift({ id: chatgroups.length + 1, currentGroupName, messages: [] })
     socket.emit('groupList', chatgroups)
+  })
+  socket.on("findGroup", (id) => {
+    const filteredGroup = chatgroups.filter(item => item.id === id)
+    socket.emit('foundGroup', filteredGroup[0].messages)
+  })
+  socket.on("newChatMessage", (data) => {
+    const { currentChatMessage,
+      groupIdentifier,
+      currentUser,
+      timeData } = data
+    const filteredGroup = chatgroups.filter(item => item.id === groupIdentifier)
+    const newMessage = {
+      id: createUniqueId(),
+      text: currentChatMessage,
+      currentUser,
+      time: `${timeData.hr}:${timeData.mins}`
+    }
+    socket.to(filteredGroup[0].currentGroupName).emit('groupMessage', newMessage)
+    filteredGroup[0].messages.push(newMessage)
+    socket.emit('groupList', chatgroups)
+    socket.emit('foundGroup', filteredGroup[0].messages)
   })
 
 })
